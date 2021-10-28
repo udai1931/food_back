178f325ba2964a78a6a36da8012b9a15d4d01710
Udai1931 <udaigupta19311@gmail.com>
2021-10-29 00:49:10 +0530

    make it better

---
+++ controller/bookingController.js
@@ -0,0 +1,44 @@
+let SK="sk_test_pL1X84CZrOSOYRkyyvKuCwR000a36t5jwK";
+const stripe=require('stripe')(SK);
+const planModel = require("../models/planModel");
+const userModel = require("../models/userModel");
+
+module.exports.createSession=async function createSession(req,res){
+    try{
+    let userId=req.body.id;
+    let planId=req.body.id;
+
+    const user = await userModel.findById(userId);
+    const plan = await planModel.findById(planId);
+
+    console.log("1234567");
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        customer_email: user.email,
+        client_refernce_id: plan.id,
+        line_items: [
+          {
+            name: plan.name,
+            description: plan.description,
+            // deploy website 
+            amount: plan.price * 100,
+            currency: "inr",
+            quantity: 1
+          }
+        ],
+        // dev => http
+        // production => https 
+        success_url: `${req.protocol}://${req.get("host")}/profile`,
+        cancel_url: `${req.protocol}://${req.get("host")}/profile`
+      })
+      
+      res.status(200).json({
+        status: "success",
+        session
+      })
+    } catch (err) {
+      res.status(500).json({
+        err: err.message
+      })
+    }
+}
